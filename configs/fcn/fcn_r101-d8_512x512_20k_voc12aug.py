b8846ba4eab47d2d8e4030b7ec8d7efb9b7a96d0
rubeea <rubeea@users.noreply.github.com>
2020-11-19 17:07:40 +0800

    Update fcn_r101-d8_512x512_20k_voc12aug.py
    PLDU config file

--- configs/fcn/fcn_r101-d8_512x512_20k_voc12aug.py
+++ configs/fcn/fcn_r101-d8_512x512_20k_voc12aug.py
@@ -1,2 +1,35 @@
 _base_ = './fcn_r50-d8_512x512_20k_voc12aug.py'
-model = dict(pretrained='open-mmlab://resnet101_v1c', backbone=dict(depth=101))
+model = dict(pretrained='open-mmlab://resnet101_v1c',
+             backbone=dict(depth=101),
+             decode_head=dict(num_classes=2),
+             auxiliary_head=dict(num_classes=2)
+             )
+
+dataset_type = 'PLDUDataset'  # Dataset type, this will be used to define the dataset.
+data_root = '../data/pldu/'
+data = dict(
+    samples_per_gpu=2,
+    workers_per_gpu=2,
+    train=dict(
+        type='PLDUDataset',
+        data_root='../data/pldu/',
+        img_dir='img_dir/train',
+        ann_dir='ann_dir/train',
+        split=None,
+        ),
+    val=dict(
+        type='PLDUDataset',
+        data_root='../data/pldu/',
+        img_dir='img_dir/val',
+        ann_dir='ann_dir/val',
+        split=None,
+        ),
+    test=dict(
+        type='PLDUDataset',
+        data_root='../data/pldu/',
+        img_dir='img_dir/val',
+        ann_dir='ann_dir/val',
+        split=None,
+        )
+)
+
