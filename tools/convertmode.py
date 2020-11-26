bc5e166427c06090be5332417664b0825178b32b
rubeea <rubeea@users.noreply.github.com>
2020-11-26 20:09:13 +0800

    Update convertmode.py
    Changed the pixels values to be in range [0,num_classes-1] as per the dataset format and then added the palette for the P mode.

--- tools/convertmode.py
+++ tools/convertmode.py
@@ -1,6 +1,7 @@
 import glob
 from PIL import Image
 import os
+import numpy as np
 
 # src_class="pldu"
 src_dir = "C:/Users/Xyedj/Desktop/datasets/wires/PLDU/train/aug_gt/0.0_0"
@@ -11,10 +12,15 @@ def convert_mode(): #convert data to 'P' mode
         filename = imgfile.split("\\")[1]  # extract filename with .png extension
         final_file_name= dst_dir+filename
         pimg = Image.open(imgfile)
+        for pixel in pimg.getdata():
+            if (pixel[0] == 255 and pixel[1] == 255 and pixel[2] == 255):  # white pixels
+                pixel=[1,1,1]
         # print(pimg.format)
         # print(pimg.size)
         # print(pimg.mode)
+        palette = [[0, 0, 0], [255, 255, 255]]
         pimg = pimg.convert("P")
+        pimg.putpalette(np.array(palette, dtype=np.uint8))
         # print(pimg.mode)
         pimg=pimg.save(final_file_name)
 
