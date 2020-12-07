79bb27d261341e51c5c013be0c8c07ad60cde810
rubeea <rubeea@users.noreply.github.com>
2020-12-07 14:41:03 +0800

    Update cross_entropy_loss.py
    setting ignore_index in binary cross entropy loss back to 255 because the images have been converted to P mode

--- mmseg/models/losses/cross_entropy_loss.py
+++ mmseg/models/losses/cross_entropy_loss.py
@@ -60,7 +60,7 @@ def binary_cross_entropy(pred,
                          reduction='mean',
                          avg_factor=None,
                          class_weight=None,
-                         ignore_index=None):
+                         ignore_index=255):
     """Calculate the binary CrossEntropy loss.
 
     Args:
