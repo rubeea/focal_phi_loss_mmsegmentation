d63e4580bdcd7272012d9895d8c88d0304fd7ca8
rubeea <rubeea@users.noreply.github.com>
2020-12-16 19:20:28 +0800

    Update __init__.py
    Tversky loss added to the list

--- mmseg/models/losses/__init__.py
+++ mmseg/models/losses/__init__.py
@@ -3,9 +3,10 @@ from .cross_entropy_loss import (CrossEntropyLoss, binary_cross_entropy,
                                  cross_entropy, mask_cross_entropy)
 from .utils import reduce_loss, weight_reduce_loss, weighted_loss
 from .focal_loss import FocalLoss 
+from .tversky_loss import (TverskyLoss, tversky) 
 
 __all__ = [
     'accuracy', 'Accuracy', 'cross_entropy', 'binary_cross_entropy',
     'mask_cross_entropy', 'CrossEntropyLoss', 'reduce_loss',
-    'weight_reduce_loss', 'weighted_loss', 'FocalLoss'
+    'weight_reduce_loss', 'weighted_loss', 'FocalLoss', 'TverskyLoss', 'tversky'
 ]
