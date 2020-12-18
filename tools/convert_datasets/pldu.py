e85ea1184bef1d213110f23f8c4ac219b9cf1393
rubeea <rubeea@users.noreply.github.com>
2020-12-19 00:29:56 +0800

    Update pldu.py

--- tools/convert_datasets/pldu.py
+++ tools/convert_datasets/pldu.py
@@ -6,9 +6,8 @@ import zipfile
 
 import cv2
 import mmcv
-from PIL import Image  
-import PIL  
-
+from PIL import Image
+import PIL
 
 
 def parse_args():
@@ -29,12 +28,13 @@ def parse_args():
 
 
 def main():
+
     args = parse_args()
     train_images_path = args.train_images_path
     train_labels_path = args.train_labels_path
     val_images_path = args.val_images_path
     val_labels_path = args.val_labels_path
-    
+
     if args.out_dir is None:
         out_dir = osp.join('data', 'pldu')
     else:
@@ -55,56 +55,54 @@ def main():
         zip_file.extractall(tmp_dir)
 
         print('Generating image training dataset...')
-        now_dir = osp.join(tmp_dir, 'images')
+        now_dir = osp.join(tmp_dir, 'train_imgs')
         for img_name in sorted(os.listdir(now_dir)):
             img = Image.open(osp.join(now_dir, img_name))
-            img= img.save(osp.join(out_dir, 'img_dir', 'train',
-                    osp.splitext(img_name)[0] +'.jpg'))
-            
-     with tempfile.TemporaryDirectory(dir=args.tmp_dir) as tmp_dir:
+            img = img.save(osp.join(out_dir, 'img_dir', 'train',
+                                    osp.splitext(img_name)[0] + '.jpg'))
+
+    with tempfile.TemporaryDirectory(dir=args.tmp_dir) as tmp_dir:
         print('Extracting val images.zip...')
         zip_file = zipfile.ZipFile(val_images_path)
         zip_file.extractall(tmp_dir)
 
         print('Generating image training dataset...')
-        now_dir = osp.join(tmp_dir, 'images')
+        now_dir = osp.join(tmp_dir, 'val_imgs')
         for img_name in sorted(os.listdir(now_dir)):
             img = Image.open(osp.join(now_dir, img_name))
-            img= img.save(osp.join(out_dir, 'img_dir', 'val',
-                    osp.splitext(img_name)[0] +'.jpg'))
-            
+            img = img.save(osp.join(out_dir, 'img_dir', 'val',
+                                    osp.splitext(img_name)[0] + '.jpg'))
 
         print('Extracting train_gt.zip...')
         zip_file = zipfile.ZipFile(train_labels_path)
         zip_file.extractall(tmp_dir)
 
         print('Generating annotations for training dataset...')
-        
+
         if osp.exists(now_dir):
             now_dir = osp.join(tmp_dir, 'train_gt')
             for img_name in sorted(os.listdir(now_dir)):
                 img = Image.open(osp.join(now_dir, img_name))
-                img=img.save(osp.join(out_dir, 'ann_dir', 'train',
-                             osp.splitext(img_name)[0] + '.png'))
-        
+                img = img.save(osp.join(out_dir, 'ann_dir', 'train',
+                                        osp.splitext(img_name)[0] + '.png'))
+
         print('Extracting val_gt.zip...')
         zip_file = zipfile.ZipFile(val_labels_path)
         zip_file.extractall(tmp_dir)
 
         print('Generating annotations for validation dataset...')
-        
+
         if osp.exists(now_dir):
             now_dir = osp.join(tmp_dir, 'val_gt')
             for img_name in sorted(os.listdir(now_dir)):
                 img = Image.open(osp.join(now_dir, img_name))
-                img=img.save(osp.join(out_dir, 'ann_dir', 'val',
-                             osp.splitext(img_name)[0] + '.png'))
-        
-        
+                img = img.save(osp.join(out_dir, 'ann_dir', 'val',
+                                        osp.splitext(img_name)[0] + '.png'))
+
         print('Removing the temporary files...')
 
-    print('Done!')
 
+print('Done!')
 
 if __name__ == '__main__':
     main()
