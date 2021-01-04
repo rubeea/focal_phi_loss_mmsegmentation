44239849520bdaf3be0670348c9ee771aa7e30af
rubeea <rubeea@users.noreply.github.com>
2021-01-04 15:55:05 +0800

    Update __init__.py
    Added ComboLoss

--- mmseg/models/losses/__init__.py
+++ mmseg/models/losses/__init__.py
@@ -4,6 +4,7 @@ from .cross_entropy_loss import (CrossEntropyLoss, binary_cross_entropy,
 from .utils import reduce_loss, weight_reduce_loss, weighted_loss
 from .focal_loss import FocalLoss 
 from .tversky_loss import (TverskyLoss, tversky) 
+from .combo_loss import ComboLoss
 
 __all__ = [
     'accuracy', 'Accuracy', 'cross_entropy', 'binary_cross_entropy',
