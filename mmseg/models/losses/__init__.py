f7c2dfdf783aa677793d5fab568627c16bb6d879
rubeea <rubeea@users.noreply.github.com>
2021-01-10 00:12:26 +0800

    Update __init__.py
    phi loss added

--- mmseg/models/losses/__init__.py
+++ mmseg/models/losses/__init__.py
@@ -3,11 +3,12 @@ from .cross_entropy_loss import (CrossEntropyLoss, binary_cross_entropy,
                                  cross_entropy, mask_cross_entropy)
 from .utils import reduce_loss, weight_reduce_loss, weighted_loss
 from .focal_loss import FocalLoss 
-from .tversky_loss import (TverskyLoss, tversky_loss) 
+from .tversky_loss import (TverskyLoss, tversky_loss)
+from .phi_loss import (PhiLoss, phi_loss)
 from .combo_loss import ComboLoss
 
 __all__ = [
     'accuracy', 'Accuracy', 'cross_entropy', 'binary_cross_entropy',
     'mask_cross_entropy', 'CrossEntropyLoss', 'reduce_loss',
-    'weight_reduce_loss', 'weighted_loss', 'FocalLoss', 'TverskyLoss', 'tversky_loss', 'ComboLoss'
+    'weight_reduce_loss', 'weighted_loss', 'FocalLoss', 'TverskyLoss', 'tversky_loss', 'PhiLoss', 'phi_loss', 'ComboLoss'
 ]
