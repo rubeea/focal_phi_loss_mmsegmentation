d3940e7a93f30d23c6c93d3ff0975b6e56f218b0
rubeea <rubeea@users.noreply.github.com>
2021-01-13 22:30:36 +0800

    Update pspnet_unet_s5-d16_256x256_40k_hrf.py

--- configs/unet/pspnet_unet_s5-d16_256x256_40k_hrf.py
+++ configs/unet/pspnet_unet_s5-d16_256x256_40k_hrf.py
@@ -1,6 +1,6 @@
 _base_ = [
-    '../_base_/models/pspnet_unet_s5-d16.py', '../_base_/datasets/hrf.py',
-    '../_base_/default_runtime.py', '../_base_/schedules/schedule_40k.py'
+    '../_base_/models/pspnet_unet_s5-d16.py', '../_base_/datasets/pascal_voc12.py',
+    '../_base_/default_runtime.py', '../_base_/schedules/schedule_20k.py'
 ]
 test_cfg = dict(crop_size=(256, 256), stride=(170, 170))
-evaluation = dict(metric='mDice')
+evaluation = dict(metric='mIoU')
