656afb8507b28bd82dae3d16ffe6dcc9a59f145b
rubeea <rubeea@users.noreply.github.com>
2021-01-18 22:20:06 +0800

    Update fcn_unet_s5-d16_256x256_40k_hrf.py

--- configs/unet/fcn_unet_s5-d16_256x256_40k_hrf.py
+++ configs/unet/fcn_unet_s5-d16_256x256_40k_hrf.py
@@ -3,4 +3,4 @@ _base_ = [
     '../_base_/default_runtime.py', '../_base_/schedules/schedule_20k.py'
 ]
 test_cfg = dict(crop_size=(256, 256), stride=(170, 170))
-evaluation = dict(metric='IoU')
+evaluation = dict(metric='mIoU')
