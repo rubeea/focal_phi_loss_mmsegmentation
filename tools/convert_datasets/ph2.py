a601eaa3f2422f4e8fd4cdfc6f987254703f316a
rubeea <rubeea@users.noreply.github.com>
2021-01-21 17:16:48 +0800

    Update ph2.py

--- tools/convert_datasets/ph2.py
+++ tools/convert_datasets/ph2.py
@@ -8,19 +8,11 @@ import mmcv
 
 def parse_args():
     parser = argparse.ArgumentParser(
-        description='Convert HRF dataset to mmsegmentation format')
-    parser.add_argument('healthy_path', help='the path of healthy.zip')
+        description='Convert PH2 dataset to mmsegmentation format')
     parser.add_argument(
-        'healthy_manualsegm_path', help='the path of healthy_manualsegm.zip')
-    parser.add_argument('glaucoma_path', help='the path of glaucoma.zip')
+        'images_path', help='the images part of PH2 dataset')
     parser.add_argument(
-        'glaucoma_manualsegm_path', help='the path of glaucoma_manualsegm.zip')
-    parser.add_argument(
-        'diabetic_retinopathy_path',
-        help='the path of diabetic_retinopathy.zip')
-    parser.add_argument(
-        'diabetic_retinopathy_manualsegm_path',
-        help='the path of diabetic_retinopathy_manualsegm.zip')
+        'labels_path', help='the labels part of PH2 dataset')
     parser.add_argument('--tmp_dir', help='path of the temporary directory')
     parser.add_argument('-o', '--out_dir', help='output path')
     args = parser.parse_args()
@@ -29,13 +21,6 @@ def parse_args():
 
 def main():
     args = parse_args()
-    images_path = [
-        args.healthy_path, args.glaucoma_path, args.diabetic_retinopathy_path
-    ]
-    annotations_path = [
-        args.healthy_manualsegm_path, args.glaucoma_manualsegm_path,
-        args.diabetic_retinopathy_manualsegm_path
-    ]
     if args.out_dir is None:
         out_dir = osp.join('data', 'ph2')
     else:
@@ -51,7 +36,7 @@ def main():
     mmcv.mkdir_or_exist(osp.join(out_dir, 'ann_dir', 'val'))
 
     print('Generating images...')
-    for now_path in images_path:
+    for now_path in args.images_path:
         with tempfile.TemporaryDirectory(dir=args.tmp_dir) as tmp_dir:
             zip_file = zipfile.ZipFile(now_path)
             zip_file.extractall(tmp_dir)
@@ -72,7 +57,7 @@ def main():
                              osp.splitext(filename)[0] + '.jpg'))
 
     print('Generating annotations...')
-    for now_path in annotations_path:
+    for now_path in args.labels_path:
         with tempfile.TemporaryDirectory(dir=args.tmp_dir) as tmp_dir:
             zip_file = zipfile.ZipFile(now_path)
             zip_file.extractall(tmp_dir)
