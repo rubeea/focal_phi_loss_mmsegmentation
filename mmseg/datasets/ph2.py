78d4dc5148f94d6dadbc4a534bce6ac211655861
rubeea <rubeea@users.noreply.github.com>
2021-01-22 21:01:22 +0800

    Update ph2.py
    Updated image loading from mmcv to PIL due to error which was being caused between loaded semantic segmentation and the model prediction

--- mmseg/datasets/ph2.py
+++ mmseg/datasets/ph2.py
@@ -1,26 +1,84 @@
+import argparse
+import os
 import os.path as osp
+import tempfile
+import zipfile
+from PIL import Image
+import mmcv
 
-from .builder import DATASETS
-from .custom import CustomDataset
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description='Convert PH2 dataset to mmsegmentation format')
+    parser.add_argument(
+        'images_path', help='the images part of PH2 dataset')
+    parser.add_argument(
+        'labels_path', help='the labels part of PH2 dataset')
+    parser.add_argument('--tmp_dir', help='path of the temporary directory')
+    parser.add_argument('-o', '--out_dir', help='output path')
+    args = parser.parse_args()
+    return args
 
 
-@DATASETS.register_module()
-class PH2Dataset(CustomDataset):
-    """PH2 dataset.
-    In segmentation map annotation for PH2, 0 stands for background, which is
-    included in 2 categories. ``reduce_zero_label`` is fixed to False. The
-    ``img_suffix`` is fixed to '.jpg' and ``seg_map_suffix`` is fixed to
-    '_.png'.
-    """
+def main():
+    args = parse_args()
+    if args.out_dir is None:
+        out_dir = osp.join('/content/pldu_mmsegmentation/data', 'ph2')
+    else:
+        out_dir = args.out_dir
 
-    CLASSES = ('background', 'lesion')
+    print('Making directories...')
+    mmcv.mkdir_or_exist(out_dir)
+    mmcv.mkdir_or_exist(osp.join(out_dir, 'img_dir'))
+    mmcv.mkdir_or_exist(osp.join(out_dir, 'img_dir','train'))
+    mmcv.mkdir_or_exist(osp.join(out_dir, 'img_dir','val'))
+    mmcv.mkdir_or_exist(osp.join(out_dir, 'ann_dir'))
+    mmcv.mkdir_or_exist(osp.join(out_dir, 'ann_dir', 'train'))
+    mmcv.mkdir_or_exist(osp.join(out_dir, 'ann_dir', 'val'))
 
-    PALETTE = [[120, 120, 120], [6, 230, 230]]
+    with tempfile.TemporaryDirectory(dir=args.tmp_dir) as tmp_dir:
+      print('Extracting images.zip...')
+      zip_file = zipfile.ZipFile(args.images_path)
+      zip_file.extractall(tmp_dir)
 
-    def __init__(self, **kwargs):
-        super(PH2Dataset, self).__init__(
-            img_suffix='.jpg',
-            seg_map_suffix='.png',
-            reduce_zero_label=False,
-            **kwargs)
-        assert osp.exists(self.img_dir)
+      now_dir = osp.join(tmp_dir, 'images')
+      TRAINING_LEN = int(len(os.listdir(now_dir))*0.8) #80% of the data for training and the remainig 20% for validation
+      
+      print('Generating image training dataset...')
+      for filename in sorted(os.listdir(now_dir))[:TRAINING_LEN]:
+        img = Image.open(osp.join(now_dir, filename))
+        img = img.save(osp.join(out_dir, 'img_dir', 'train',
+                                    osp.splitext(filename)[0] + '.jpg'))
+      
+      print('Generating image validation dataset...')
+      for filename in sorted(os.listdir(now_dir))[TRAINING_LEN:]:
+        img = Image.open(osp.join(now_dir, filename))
+        img = img.save(osp.join(out_dir, 'img_dir', 'val',
+                                    osp.splitext(filename)[0] + '.jpg'))
+
+   
+    with tempfile.TemporaryDirectory(dir=args.tmp_dir) as tmp_dir:
+      print('Extracting labels.zip...')
+      zip_file = zipfile.ZipFile(args.labels_path)
+      zip_file.extractall(tmp_dir)
+
+      now_dir = osp.join(tmp_dir, 'gt')
+      TRAINING_LEN = int(len(os.listdir(now_dir))*0.8) #80% of the data for training and the remainig 20% for validation
+
+      print('Generating annotations for training dataset...')
+      for filename in sorted(os.listdir(now_dir))[:TRAINING_LEN]:
+        img = Image.open(osp.join(now_dir, filename))
+                
+        img = img.save(osp.join(out_dir, 'ann_dir', 'train',
+                                    osp.splitext(filename)[0] + '.png'))
+
+      print('Generating annotations for validation dataset...')
+      for filename in sorted(os.listdir(now_dir))[TRAINING_LEN:]:
+        img = Image.open(osp.join(now_dir, filename))
+        img = img.save(osp.join(out_dir, 'ann_dir', 'val',
+                                    osp.splitext(filename)[0] + '.png'))
+
+    print('Done!')
+
+
+if __name__ == '__main__':
+    main()
