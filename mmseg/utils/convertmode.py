9643715b7bac5b482745c54b3f78abf2e8784397
rubeea <rubeea@users.noreply.github.com>
2021-02-28 01:56:09 +0800

    Update convertmode.py

--- mmseg/utils/convertmode.py
+++ mmseg/utils/convertmode.py
@@ -1,11 +1,15 @@
+/*
+Script for converting the mask images to the mmsegmentation form
+*/
+
 import glob
 from PIL import Image
 import os
 import numpy as np
 
 # src_class="pldu"
-src_dir = "C:/Users/Xyedj/Desktop/datasets/wires/PLDU/gt/aug_gt/90.0_0"
-dst_dir = "C:/Users/Xyedj/Desktop/datasets/wires/PLDU/gt/pmode/"
+src_dir = "C:/Users/Xyedj/Desktop/datasets/wires/PLDU/gt/aug_gt/90.0_0" #image dir with default mask images 
+dst_dir = "C:/Users/Xyedj/Desktop/datasets/wires/PLDU/gt/pmode/" #image dir to store the converted mask images in mmsegmentation format
 
 def convert_mode(): #convert data to 'P' mode
     for imgfile in glob.iglob((os.path.join(src_dir, "*.png"))):
