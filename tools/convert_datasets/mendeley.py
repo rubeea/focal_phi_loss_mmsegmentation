4b4dc031d7a63eb334b077a7e6e7f84ef0166d0c
rubeea <rubeea@users.noreply.github.com>
2021-02-28 01:47:02 +0800

    Update mendeley.py
    updated pldu_mmsegmentation to focal_phi_loss_mmsegmentation

--- tools/convert_datasets/mendeley.py
+++ tools/convert_datasets/mendeley.py
@@ -36,7 +36,7 @@ def main():
     val_labels_path = args.val_labels_path
 
     if args.out_dir is None:
-        out_dir = osp.join('/content/pldu_mmsegmentation/data', 'mendeley')
+        out_dir = osp.join('/content/focal_phi_loss_mmsegmentation/data', 'mendeley')
     else:
         out_dir = args.out_dir
 
